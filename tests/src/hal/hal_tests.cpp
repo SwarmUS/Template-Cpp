83c19596bddba2873edfed75bd3c85d08256cae6
xgroleau <grox2901@usherbrooke.ca>
2020-06-15 14:41:18 -0400

    tests are working

--- tests/src/hal/hal_tests.cpp
+++ tests/src/hal/hal_tests.cpp
@@ -1,19 +1,26 @@
 #include <gtest/gtest.h>
 #include "../../../src/hal/hal.hpp"
 
-class HalTest : public testing::Test {
+class HalTestFixture : public testing::Test {
+
+public:
+    Hal hal;
 
 protected:
-    Hal hal_;
 
     void SetUp() override { 
-        hal_ = Hal();
+        hal = Hal();
     }
 
     void TearDown() override {
     }
 };
 
-TEST_F(HalTest, Register_Read_Address1_Returns1) {
-  ASSERT_EQ(hal_.readRegister(1), 1);
+TEST_F(HalTestFixture, Register_Read_Address1_Returns1) {
+  ASSERT_EQ(hal.readRegister(1), 1);
 }
+
+TEST_F(HalTestFixture, Register_Read_AddressNeg_Returns1) {
+  ASSERT_EQ(hal.readRegister(-1), 1);
+}
+
